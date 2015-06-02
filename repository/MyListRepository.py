518a0d8d8ddf4dd571b3cd28e8759abfe23d04a9
roronya <roronya628@gmail.com>
2015-06-02 16:21:16 +0900

    create is_exist()

--- repository/MyListRepository.py
+++ repository/MyListRepository.py
@@ -13,6 +13,12 @@ class MyListRepository:
     def find(self, id):
         return self._session.query(MyList).filter(MyList.id == id).one()
 
+    def is_exist(self, mylist_id):
+        if 0 < len(self._session.query(MyList).filter(MyList.id == mylist_id).all()):
+            return True
+        else:
+            return False
+
     def save(self, mylist):
         self._session.add(mylist)
         self._session.commmit()
