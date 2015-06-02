9e69f97a479d0455170a19cf632a640424d61ee4
roronya <roronya628@gmail.com>
2015-06-02 10:28:41 +0900

    create MyListRepository

---
+++ repository/MyListRepository.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/../entity')
+
+from sqlalchemy.orm import sessionmaker
+from Base import *
+from MyList import MyList
+
+class MyListRepository:
+    def __init__(self):
+        self._session = sessionmaker(bind=engine)()
+
+    def find(self, id):
+        return self._session.query(MyList).filter(MyList.id == id).one()
+
+    def save(self, mylist):
+        self._session.add(mylist)
+        self._session.commmit()
